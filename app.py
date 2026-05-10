26eddb53b0bd152c7a1b5ba907781983360f8ea5
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2026-05-10 16:46:03 +0530

    added login and register

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,flash,redirect
+from form import RegistrationForm,LoginForm
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
 
 posts=[
     {
@@ -25,6 +31,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html",title="About")
+
+@app.route("/register" ,methods=['POST','GET'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'user created successfully for {form.username.data}','success')
+        return redirect(url_for('home'))
+    return render_template("register.html",title="Register",form=form)
+
+@app.route("/login",methods=['POST','GET'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == 'admin':
+            flash('You are logged in successfully!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid email or password! Please Try Again','danger')
+    return render_template("login.html",title="Login",form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
